312f1fe901777d4836039bbb2a42934ed21317d3
rex_yan <rex_yan@126.com>
2018-09-16 22:39:48 +0800

    class+注释+不转义+htmlFor

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import './style.css'
 
 class TodoList extends Component {
 
@@ -13,17 +14,26 @@ class TodoList extends Component {
   render () {
     return (
         <Fragment> 
+          {/* htmlFor */}
+          <label htmlFor='inputArea'>请输入内容</label>
           <input 
+            id='inputArea'
+            // 添加class
+            className='input'
             value={ this.state.inputValue }    
             onChange={ this.onChangeHandler.bind(this) } 
           /> 
           <button onClick={ this.handleBtnClick.bind(this) }>提交</button>
         	<ul>
-          		{ 
-                // 使用map将数组进行循环，value为值，index为索引
+          		{
                 this.state.list.map((value, index) =>{ 
-                    // 为每一个循环的值都加上一个key值,且为每一个循环项都加上一个handleBtnDeleteClick事件,并将index传入函数
-                    return <li key={ index } onClick={ this.handleBtnDeleteClick.bind(this, index) }> { value } </li>
+                    return <li 
+                              key={ index } 
+                              onClick={ this.handleBtnDeleteClick.bind(this, index) }
+                              // 不转义html标签，渲染出原始内容
+                              dangerouslySetInnerHTML={{__html:value}}
+                              >
+                          </li>
                 })
               }
         	</ul>
@@ -40,22 +50,15 @@ class TodoList extends Component {
 
   handleBtnClick(){
     this.setState({
-      // ...this.state.list为展开运算符，相当于将this.state.list的值全部放在此处。然后在加上一个this.state.inputValue的值
-      // 所以list的值就等于this.state.list + this.state.inputValue
       list: [...this.state.list, this.state.inputValue],
-      // 每次都将input中的值清空
       inputValue: ''
     })
   }
 
   handleBtnDeleteClick(e){
-    // 获取传递过来的下标值
     console.log(e)
-    // 将当前的this.state.list赋值给一个局部变量list。为什么要这么做呢？因为react不允许我们直接修改state中的值
     const list = [...this.state.list]
-    // 将list的值从e的位置处开始删除一个
     list.splice(e, 1)
-    // 将删除后的值赋予state中的list
     this.setState({
       list:list
     })
