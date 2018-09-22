581145e836969c26ee5e9d25542719ba2ecce4aa
rex_yan <rex_yan@126.com>
2018-09-22 11:41:19 +0800

    PropTypes与DefaultProps

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react' 
+import PropTypes from 'prop-types'
 
 class TodoItem extends Component {
 	constructor(props){
 		super(props)
-		this.deleteFunc = this.deleteFunc.bind(this) // 在构造中进行this绑定
+		this.deleteFunc = this.deleteFunc.bind(this) 
 	} 
 
 	render() {
-			const { content } = this.props; // 利用ES6中解构赋值的方法。等价于 const content = this.props.content;
-			return (<li onClick={ this.deleteFunc } > { content } </li>)
+			const { content, default_props } = this.props; 
+			return (<li onClick={ this.deleteFunc } > { default_props } - { content } </li>)
 		}
 	
 	deleteFunc(){
-		const { index, deleteItem } = this.props; // 解构赋值
+		const { index, deleteItem } = this.props; 
 		deleteItem({ index })
 	}
 }
+
+// 为父组件传递来的值进行校验
+TodoItem.propTypes = {
+	// 字符串类型，且必传
+	content: PropTypes.string.isRequired, 
+	// 函数类型
+	deleteItem: PropTypes.func,
+	// 数字类型
+	index: PropTypes.number,
+	default_props: PropTypes.string.isRequired, 
+
+	// 更多类型https://reactjs.org/docs/typechecking-with-proptypes.html#___gatsby
+}
+
+// 设置默认值
+TodoItem.defaultProps = {
+	default_props: 'xxxx', 
+}
+
+
 
 export default TodoItem;
--- src/TodoList.js
+++ src/TodoList.js
@@ -11,7 +11,7 @@ class TodoList extends Component {
       list:[]
     }
 
-    this.onChangeHandler = this.onChangeHandler.bind(this) // 在构造中绑定this指向
+    this.onChangeHandler = this.onChangeHandler.bind(this) 
     this.handleBtnClick = this.handleBtnClick.bind(this)
     this.handleBtnDeleteClick = this.handleBtnDeleteClick.bind(this)
   }
@@ -34,7 +34,6 @@ class TodoList extends Component {
       )
   }
 
-  // 在jsx中写map之类的不太好，所以将其封装为一个函数，返回即可
   getTodoItem(){
     return this.state.list.map((value, index) =>{ 
         return  <TodoItem content={ value } key={ index } index={ index } deleteItem={ this.handleBtnDeleteClick } > </TodoItem>      
@@ -42,13 +41,6 @@ class TodoList extends Component {
   }
 
   onChangeHandler(e){
-    // console.log(e.target.value)  
-    // this.setState({              
-    //    inputValue: e.target.value 
-    // })
-
-    // 新版本的react给State赋值的时候可以不是以对象的方式了，而是以函数的方式
-    // 注意，这里在使用函数方式进行setState时，需要先将e.target.value 的值赋给一个变量，因为这里是异步的设置数据
     const value = e.target.value 
     this.setState(() => ({
          inputValue: value
@@ -56,13 +48,6 @@ class TodoList extends Component {
   }
 
   handleBtnClick(){
-    // this.setState({
-    //  list: [...this.state.list, this.state.inputValue],
-    //  inputValue: ''
-    // })
-
-    // 这里同样使用函数的方式进行setState，setState可以接受一个参数，这个参数是未修改前的setState的值
-    // 那我们就可以将this.state替换为preState
     this.setState((preState)=>{
       return{
         list: [...preState.list, preState.inputValue],
@@ -73,16 +58,9 @@ class TodoList extends Component {
 
   handleBtnDeleteClick(e){
     console.log(e)
-    // const list = [...this.state.list]
-    // list.splice(e, 1)
-    // this.setState({
-    //  list:list
-    //})
-
     this.setState((preState)=> {
         const list = [...this.state.list]
         list.splice(e, 1)
-        // 这里相当于return { list：list } 在ES6中简写为 return { list }
         return {
           list
         }
