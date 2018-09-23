40d0ce9fe014405e147ca9ce264b86b6e186fe63
rex_yan <rex_yan@126.com>
2018-09-23 16:05:42 +0800

    Action和Reducer

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,26 +3,65 @@ import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd';
 import store from './store'
 
+
 class TodoList extends Component{
 	constructor(props){
 		super(props);
 		this.state = store.getState()
+
+		// 绑定thid指向
+		this.handleChange = this.handleChange.bind(this);
+		this.handleStoreChange = this.handleStoreChange.bind(this)
+		this.handleBtnClick = this.handleBtnClick.bind(this)
+
+		// 订阅store，如果store有变化，就触发handleStoreChange函数
+		store.subscribe(this.handleStoreChange)
 	}
 
 	render(){
 		return (
 			<div>
-				<Input value={ this.state.inputVlaue } placeholder="在此输入文本..." style={{ width:'300px', marginRight:'10px' }}/>
-				<Button type="primary">提交</Button>
+				<Input value={ this.state.inputVlaue } onChange = { this.handleChange } placeholder="在此输入文本..." style={{ width:'300px', marginRight:'10px' }}/>
+				<Button type="primary" onClick={ this.handleBtnClick }>提交</Button>
 				<List
-				  style={{ width:'300px', marginTop
-				  :'10px' }}
+				  style={{ width:'300px', marginTop:'10px' }}
 			      bordered
 			      dataSource={ this.state.list }
-			      renderItem={item => (<List.Item>{item}</List.Item>)}
+			      renderItem={(item, index) => (<List.Item onClick={ this.handleBtnDelete.bind(this, index) }>{item}</List.Item>)}
     			/>
 			</div>
 		)
+	}
+
+	handleChange(e){
+		// 定义一个action，给定一个action的类型，以及数据
+		const action = {
+			type: 'change_input_value',
+			value: e.target.value
+		}
+		// 调用dispatch，将action传递给store
+		store.dispatch(action)
+	}
+
+	handleBtnClick(){
+		const action = {
+			type:'add_todo_item'
+		}
+		store.dispatch(action)
+	}
+
+	
+	handleBtnDelete(index){
+		const action = {
+			type:'delete_todo_item',
+			index // 相当于index：index
+		}
+		store.dispatch(action)
+	}
+
+	handleStoreChange(){
+		// 将store中的数据进行同步
+		this.setState(store.getState());
 	}
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,31 @@
 const defaultState = {
-	inputVlaue: 'test',
-	list: [1,2,3]
+	inputVlaue: '',
+	list: []
 }
 
-// reducer 的返回为一个函数，函数的第一个参数是管理的数据，默认值为defaultState。第二个参数的action
 export default (state = defaultState, action) => {
+	// 匹配action type
+	if (action.type === 'change_input_value'){
+		// 将state数据进行一次深拷贝(因为reducer可以接收state，但是绝不能修改state)
+		const newState = JSON.parse(JSON.stringify(state));
+		// 改变其中inputVlaue的值为 action中的value
+		newState.inputVlaue = action.value
+		return newState;
+	}
+
+	if (action.type === 'add_todo_item'){
+		const newState = JSON.parse(JSON.stringify(state));
+		newState.list.push(newState.inputVlaue)
+		newState.inputVlaue = '';
+		return newState;
+	}
+
+	if (action.type === 'delete_todo_item'){
+		const newState = JSON.parse(JSON.stringify(state));
+		// 删除一项
+		newState.list.splice(action.index, 1);
+		return newState;
+	}
+
 	return state;
 }
