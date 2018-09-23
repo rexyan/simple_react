7d0274032e31ab78e530bc4482e13e27f79d7af0
rex_yan <rex_yan@126.com>
2018-09-23 11:06:57 +0800

    Redux_Flow_Antd

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,82 +1,31 @@
-import React, { Component, Fragment } from 'react';
-import TodoItem from './TodoItem';
-import axios from 'axios';
-import './style.css';
+import React, { Component } from 'react'
+import 'antd/dist/antd.css'
+import { Input, Button, List } from 'antd';
 
-class TodoList extends Component {
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+];
 
-  constructor(props){
-    super(props);
-    this.state = {  
-      inputValue: '',
-      list:[]
-    }
 
-    this.onChangeHandler = this.onChangeHandler.bind(this) 
-    this.handleBtnClick = this.handleBtnClick.bind(this)
-    this.handleBtnDeleteClick = this.handleBtnDeleteClick.bind(this)
-  }
-
-  render () {
-    return (
-        <Fragment> 
-          <label htmlFor='inputArea'>请输入内容</label>
-          <input 
-            id='inputArea'
-            className='input'
-            value={ this.state.inputValue }    
-            onChange={ this.onChangeHandler } 
-          /> 
-          <button onClick={ this.handleBtnClick }>提交</button>
-          <ul>
-              { this.getTodoItem() }  
-          </ul>
-        </Fragment>
-      )
-  }
-
-  getTodoItem(){
-    return this.state.list.map((value, index) =>{ 
-        return  <TodoItem content={ value } key={ index } index={ index } deleteItem={ this.handleBtnDeleteClick } > </TodoItem>      
-    })
-  }
-
-  onChangeHandler(e){
-    const value = e.target.value 
-    this.setState(() => ({
-         inputValue: value
-     }))
-  }
-
-  handleBtnClick(){
-    this.setState((preState)=>{
-      return{
-        list: [...preState.list, preState.inputValue],
-        inputValue: ''
-      }
-    })
-  }
-
-  handleBtnDeleteClick(e){
-    console.log(e)
-    this.setState((preState)=> {
-        const list = [...this.state.list]
-        list.splice(e, 1)
-        return {
-          list
-        }
-    })
-  }
-
-  // 页面加载完成后发起ajax请求
-  componentDidMount(){
-    axios.get('http://www.baidu.com').then(()=>{
-      alert('执行成功')
-    }).catch(()=>{
-      alert('执行失败')
-    })
-  }
-
+class TodoList extends Component{
+	render(){
+		return (
+			<div>
+				<Input placeholder="在此输入文本..." style={{ width:'300px', marginRight:'10px' }}/>
+				<Button type="primary">提交</Button>
+				<List
+				  style={{ width:'300px', marginTop:'10px' }}
+			      bordered
+			      dataSource={data}
+			      renderItem={item => (<List.Item>{item}</List.Item>)}
+    			/>
+			</div>
+		)
+	}
 }
 
 export default TodoList;
